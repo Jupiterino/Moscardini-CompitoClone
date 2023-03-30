d63aa238adc8602f534fde67fd317c50dbc9035b
Giovanni <giovahahah@gmail.com>
2023-03-30 08:38:17 +0200

    errore risolto in stampa

--- stampa.cpp
+++ stampa.cpp
@@ -8,12 +8,13 @@ GIOVANNI-MOSCARDINI-30/03/2023
 using namespace std;
 int main()
 {
-  int i=10 ,n;
+  int i=0 ,n;
   cout<<"Quante volte voi stampare la frase? ";
   cin>>n;
   while (i<n)
   {
    cout<<"Bada come la fuma"<<endl; 
+   n=n-1;
   }
   system("pause");
 }
